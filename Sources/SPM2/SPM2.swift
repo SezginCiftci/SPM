7f4666f917fea2793c8f5a39ff0cbfbc05ebd23a
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2024-08-10 16:00:47 +0300

    Initial Commit.

---
+++ Sources/SPM2/SPM2.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class MyClass {
+    
+    static public func printLog(_ text: String) {
+        print("---- \(text) ----")
+    }
+}
